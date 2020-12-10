4e23b5a9260e81ff1483499a8f07a73f3387fa28
TeonMehta <teonmehta@gmail.com>
2020-12-10 10:52:12 -0500

    adding file base to github

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
-import {Route, Switch} from "react-router-dom";
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./views/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
-    return (
-         <>
-            PSYCH0
-        </>
-             );
-    }
+  return (
+    <BrowserRouter>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/register">
+        <Register />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      PSYCH0
+    </BrowserRouter>
+  );
+}
 
 export default App;
